20ed0faf94604dccd77e42f8ffcb862a1952d611
Morgan O'Carroll <m.ocarroll@lancaster.ac.uk>
2022-06-20 11:21:14 +0100

    Bot test example

---
+++ example_bot.py
@@ -0,0 +1,22 @@
+# Example bot for testing
+
+import discord                          ## Registers the library
+        
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message():
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('your token here')
