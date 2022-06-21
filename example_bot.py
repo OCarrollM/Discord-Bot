aaa78c12de21a2862e05895fe657693c506433e4
Morgan O'Carroll <m.ocarroll@lancaster.ac.uk>
2022-06-21 13:21:15 +0100

    Added embed message

--- example_bot.py
+++ example_bot.py
@@ -19,12 +19,25 @@ async def on_disconnect():
 async def on_message(message):
     
     if message.content == 'pumps':
-            bot_channel = client.get_channel(988388873363161128)
-            await bot_channel.send('pumps?!?!')
+        bot_channel = client.get_channel(988388873363161128)
+        await bot_channel.send('pumps?!?!')
 
     if message.content == 'morb':
         bot_channel = client.get_channel(988388873363161128)
         await bot_channel.send('It\'s morbin time')
+    
+    if message.content == 'embed':
+        bot_channel = client.get_channel(988388873363161128)
+
+        myEmbed = discord.Embed(title="Current version", description="Bot version 1.0", color=0xff0000)
+        myEmbed.add_field(name="Version code: ", value="v1.0.0", inline=False)
+        myEmbed.add_field(name="Date released: ", value="June 21st 2022", inline=False)
+        myEmbed.set_footer(text="This is a sample footer")
+        myEmbed.set_author(name="Morgan")
+
+        await bot_channel.send(embed=myEmbed)
+
+
 
 # Run client on server
-client.run('OTg4Mzg5MTYxMDg0MDA2NDQw.GwfPEf.PGvVp_fKxDNTU1449HoEVUYczliT5hmYZ0gV2k')
+client.run('')
